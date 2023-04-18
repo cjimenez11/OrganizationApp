28fc45b249dd0cf18f78c625ee906b9f4be6dc50
Carlos Jimenez <cjimenez11@mail.sfsu.edu>
2023-04-18 10:28:17 -0700

    unfinished note creation login and registration successful

--- application/back_end/models/NoteModel.js
+++ application/back_end/models/NoteModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const noteSchema = mongoose.Schema({
+const noteSchema = new mongoose.Schema({
     title: {type: String, required: true},
     body: {type: String, required: true},
     user: {type: String, required: true},
