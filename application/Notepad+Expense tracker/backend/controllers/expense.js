f1c93cedaed4f702f8c2eee0f1b1024e898a9a1e
chrisf725 <chfrancisco725@yahoo.com>
2023-05-03 08:58:42 -0700

    Added unit tests for expense, income, and notes

--- application/Notepad+Expense tracker/backend/controllers/expense.js
+++ application/Notepad+Expense tracker/backend/controllers/expense.js
@@ -40,11 +40,17 @@ exports.getExpense = async (req, res) =>{
 
 exports.deleteExpense = async (req, res) =>{
     const {id} = req.params;
-    ExpenseSchema.findByIdAndDelete(id)
-        .then((income) =>{
-            res.status(200).json({message: 'Expense Deleted'})
-        })
-        .catch((err) =>{
-            res.status(500).json({message: 'Server Error'})
-        })
+    // ExpenseSchema.findByIdAndDelete(id)
+    //     .then((income) =>{
+    //         res.status(200).json({message: 'Expense Deleted'})
+    //     })
+    //     .catch((err) =>{
+    //         res.status(500).json({message: 'Server Error'})
+    //     })
+    try {
+        await ExpenseSchema.findByIdAndDelete(id);
+        res.status(200).json({ message: "Expense Deleted" });
+      } catch (err) {
+        res.status(500).json({ message: "Server Error" });
+      }
 }
